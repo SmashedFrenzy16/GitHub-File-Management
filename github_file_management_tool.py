2c564ebd35ade74a2ae7bfe7c4a90da4fe971d19
SmashedFrenzy16 <smashedfrenzy16@gmail.com>
2024-07-05 16:32:46 +0100

    Updating file with some code

--- github_file_management_tool.py
+++ github_file_management_tool.py
@@ -1,1 +1,23 @@
+from github import *
 
+token = input("Enter in your GitHub access token: ")
+
+a = Auth.Token(token)
+
+git = Github(auth=a)
+
+repo = input("Enter in your repository path (e.g. User/RepoName): ")
+
+final_repo = git.get_repo(repo)
+
+def create_file():
+
+    file = input("Enter in the file name and extension that you want to create: ")
+
+    message = input("Enter in commit message: ")
+
+    content = input("Enter file cotents: ")
+
+    branch = input("Enter the branch name you want to create this file in: ")
+
+    final_repo.create_file(file, message, content, branch)

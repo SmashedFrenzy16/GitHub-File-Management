226c92a2dcf1a9a76fb23d0a329a5aebf5b4b52d
SmashedFrenzy16 <smashedfrenzy16@gmail.com>
2024-07-05 16:39:53 +0100

    Update github_file_management_tool.py with delete_file function

--- github_file_management_tool.py
+++ github_file_management_tool.py
@@ -21,3 +21,15 @@ def create_file():
     branch = input("Enter the branch name you want to create this file in: ")
 
     final_repo.create_file(file, message, content, branch)
+
+def delete_file():
+
+    file_to_delete = input("Enter in the file name and extension of the file you want to delete: ")
+
+    message = input("Enter in commit message: ")
+
+    branch = input("Enter the branch name you want to delete this file in: ")
+
+    delete_info = final_repo.get_contents(file_to_delete, ref="test")
+
+    final_repo.delete_file(delete_info.path, message, delete_info.sha, branch=branch)

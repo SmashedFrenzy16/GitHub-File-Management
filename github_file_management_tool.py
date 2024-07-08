c6997d5a17897f8b53e72ec12ed89d07ecb4f097
SmashedFrenzy16 <smashedfrenzy16@gmail.com>
2024-07-08 13:48:49 +0100

    Adding exception cases to functions

--- github_file_management_tool.py
+++ github_file_management_tool.py
@@ -12,36 +12,55 @@ final_repo = git.get_repo(repo)
 
 def create_file():
 
-    file = input("Enter in the file name and extension that you want to create: ")
+    try:
 
-    message = input("Enter in commit message: ")
+        file = input("Enter in the file name and extension that you want to create: ")
 
-    content = input("Enter file cotents: ")
+        message = input("Enter in commit message: ")
 
-    branch = input("Enter the branch name you want to create this file in: ")
+        content = input("Enter file cotents: ")
 
-    final_repo.create_file(file, message, content, branch)
+        branch = input("Enter the branch name you want to create this file in: ")
+
+        final_repo.create_file(file, message, content, branch)
+    
+    except Exception as e:
+
+        print(f"The following error occured: {e}")
 
 def delete_file():
 
-    file_to_delete = input("Enter in the file name and extension of the file you want to delete: ")
+    try:
 
-    message = input("Enter in commit message: ")
 
-    branch = input("Enter the branch name you want to delete this file in: ")
+        file_to_delete = input("Enter in the file name and extension of the file you want to delete: ")
 
-    delete_info = final_repo.get_contents(file_to_delete, ref="test")
+        message = input("Enter in commit message: ")
 
-    final_repo.delete_file(delete_info.path, message, delete_info.sha, branch=branch)
+        branch = input("Enter the branch name you want to delete this file in: ")
+
+        delete_info = final_repo.get_contents(file_to_delete, ref="test")
+
+        final_repo.delete_file(delete_info.path, message, delete_info.sha, branch=branch)
+
+    except Exception as e:
+
+        print(f"The following error occured: {e}")
 
 def update_file():
 
-    file_to_update = input("Enter in the file name with extension that you want to update: ")
+    try:
 
-    c = final_repo.get_contents(file_to_update, ref="test")
+        file_to_update = input("Enter in the file name with extension that you want to update: ")
 
-    message = input("Enter in commit message: ")
+        c = final_repo.get_contents(file_to_update, ref="test")
 
-    branch = input("Enter the branch name you want to update this file in: ")
+        message = input("Enter in commit message: ")
 
-    final_repo.update_file(c.path, message, "test", c.sha, branch=branch)
+        branch = input("Enter the branch name you want to update this file in: ")
+
+        final_repo.update_file(c.path, message, "test", c.sha, branch=branch)
+
+    except Exception as e:
+            
+            print(f"The following error occured: {e}")
